cfd1433ef7ed5205be6254aab29384068699c318
chiru <chiranjeevi8639183417@gmail.com>
2026-02-13 11:02:12 +0530

    Add files via upload

---
+++ day 2 io operations and type convertions.py
@@ -0,0 +1,32 @@
+"""# read input numbers from user
+num = input()
+print(type(num))# checking the type of number
+print(num)"""
+
+"""#implicit type cinvertion
+
+num = int(input())
+val = float(input())
+print(type(num1), type(val)) # check the type of number
+res = num1 + value
+print(res)"""
+"""
+num1 = input() # check the type of number
+
+print(type(num1)) # checking the type of number
+res = num1 + 10 #typeerror: can only concatenate str(not "int") to str
+print(res)""" 
+"""
+# to overcome the above problem using explicit type convertion
+num1 = input() # read string type number
+
+print(type(num1)) # checking the type of number
+res = int(num1) + 10 #typeError can only concatenate str(not "int")to str
+print(res)"""
+"""
+# convertin integer into float,sting,boolean
+
+num=12
+print(float(num))
+print(str(num))
+print(bool(num))"""
