bbc3c642c6af6481bad958899e15191bd5253435
chiru <chiranjeevi8639183417@gmail.com>
2026-02-14 20:45:52 +0530

    Add files via upload
    this is the codes which is used for operators to find the type of number, is it divisible by any number, if statement, else statement, checking eligibility of division, etc.

---
+++ day 3 conditional statements.py
@@ -0,0 +1,25 @@
+"""# check if number is zero
+num = int(input())
+#
+if num == 0:
+    print("number is zero")
+print("program done")"""
+"""
+#check if number is zero
+num = int(input())
+#
+if num == 0:
+    print("number is not zero")
+else:
+    print("Number is not zero")
+
+print("Programe done")"""
+
+# read the two numbers find the bigger number
+n = int(input())
+m = int(input())
+if n>m:
+    print("n is big")
+else:
+    print("m is big")
+print("Program done")
---
+++ day 3 even or odd, divisiblity.py
@@ -0,0 +1,31 @@
+"""# find whether the number is odd or even
+num = int(input())
+if (num%2)==0:
+    print(" n is odd number ")
+else:
+    print(" n is even number ")
+    print("program done")"""
+
+"""#find the number divisible by 5 or not
+num=int(input())
+if (num/5)==0:
+    print("number divisible by 5")
+else:
+    print(" number not divisible by 5")
+    print("program is done")"""
+"""
+#check the number is divisible with 5 and 3 or not
+num=int(input())
+if (num/5,3)==0:
+    print("number is diviible with 5&3")
+else:
+    print("number is not divisible with 5&3")
+    print("program done")"""
+"""
+#check number divisible with any one number of 4 or 6 are not
+num=int(input())
+if (num/12)==0:
+    print(" check number divisible with any one number")
+else:
+    print(" check number not divisible with any one number")
+    print("program done")"""
