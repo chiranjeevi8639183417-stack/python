93a46be8e6c4fa16046a0b624e48a9230d531398
chiru <chiranjeevi8639183417@gmail.com>
2026-02-23 20:27:03 +0530

    strings
    how the indexing  works in string to check the given input is printed correct

---
+++ string day 7.py
@@ -0,0 +1,13 @@
+day 7
+s="codegnan"
+print (s[0:1:2])
+print (s[3:4:5])
+print (s[4:5:7])
+print(s[6:10])
+print(s[5:8:-1])
+print(s[::1])
+print(s[1:])
+print(s[::4])
+print(s[1:-1])
+print(s[-2:5:1])
+print(s[6:5:1])
